03d334ac4bafdbd9c220e12e9816117bf9346d7d
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-04-03 10:02:57 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import realmConfig from "./realmConfig";
+import FailedJobSchema from "./src/local-db/FailedJobSchema";
+import JobSchema from "./src/local-db/JobSchema";
+
+export {
+    realmConfig,
+    FailedJobSchema,
+    JobSchema,
+};
